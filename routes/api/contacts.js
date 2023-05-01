07be0732f46c79313dcc261d2a210d18ac34a12b
VitaliiSmyrnov <vitaliy.smirnov@ukr.net>
2023-05-01 22:35:56 +0300

    проміжковий коміт

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,43 @@
-const express = require('express')
+const express = require("express");
+const contacts = require("../../models/contacts");
 
-const router = express.Router()
+const router = express.Router();
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/", async (req, res, next) => {
+  try {
+    const allContacts = await contacts.listContacts();
+    res.json(allContacts);
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
+  }
+});
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/:contactId", async (req, res, next) => {
+  try {
+    const { contactId } = req.params;
+    const oneContact = await contacts.getContactById(contactId);
+    if (!oneContact) {
+      const error = new Error("Not found");
+      error.status = 404;
+      throw error;
+    }
+    res.json(oneContact);
+  } catch (error) {
+    const { status = 500, message = "Server error" } = error;
+    res.status(status).json({ message });
+  }
+});
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.post("/", async (req, res, next) => {
+  res.json({ message: "template message" });
+});
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.delete("/:contactId", async (req, res, next) => {
+  res.json({ message: "template message" });
+});
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.put("/:contactId", async (req, res, next) => {
+  res.json({ message: "template message" });
+});
 
-module.exports = router
+module.exports = router;
