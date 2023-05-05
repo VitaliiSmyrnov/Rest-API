c533648e161abbe1dc28e7c44db5eebea1f524cb
VitaliiSmyrnov <vitaliy.smirnov@ukr.net>
2023-05-05 18:13:10 +0300

    fix contact Schema

--- schemas/contacts.js
+++ schemas/contacts.js
@@ -4,6 +4,7 @@ const addContactSchema = Joi.object({
   name: Joi.string().required(),
   email: Joi.string().required(),
   phone: Joi.string().required(),
+  favorite: Joi.boolean(),
 });
 
 const updStatusContactSchema = Joi.object({
