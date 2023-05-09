ca55cc4d6d7396e3638b6f2391b2d7e8ec9caaef
VitaliiSmyrnov <vitaliy.smirnov@ukr.net>
2023-05-09 22:21:36 +0300

    add to Joi schema subscription

--- models/user.js
+++ models/user.js
@@ -38,6 +38,7 @@ const User = model("user", userSchema);
 const registerSchema = Joi.object({
   email: Joi.string().pattern(emailRegexp).required(),
   password: Joi.string().min(6).required(),
+  subscription: Joi.string(),
 });
 
 const loginSchema = Joi.object({
