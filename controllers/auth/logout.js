32af463931ffb811f1c95df7321a09a6582c7dcf
VitaliiSmyrnov <vitaliy.smirnov@ukr.net>
2023-05-10 17:45:18 +0300

    changes

--- controllers/auth/logout.js
+++ controllers/auth/logout.js
@@ -6,10 +6,7 @@ const logout = async (req, res) => {
   const { _id } = req.user;
   await User.findByIdAndUpdate(_id, { token: "" });
 
-  res.status(204).json({
-    code: 204,
-    message: "Logout success",
-  });
+  res.status(204).send();
 };
 
 module.exports = ctrlWrapper(logout);
--- controllers/contacts/addContact.js
+++ controllers/contacts/addContact.js
@@ -3,7 +3,10 @@ const { ctrlWrapper } = require("../../helpers");
 
 const addContact = async (req, res) => {
   const { _id: owner } = req.user;
-  const newContact = await Contact.create({ ...req.body, owner });
+  const newContact = await Contact.create(
+    { ...req.body, owner },
+    "-createAt -updateAt"
+  );
 
   res.status(201).json({ code: 201, data: newContact });
 };
